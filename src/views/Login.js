85121e97ac98f383fcabb4d149c88f421be86af4
Vitor Martins <vitorfigmartins@gmail.com>
2020-02-04 21:06:08 -0300

    Validating email and password; Redirecting to home screen

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,9 +1,21 @@
 import React, { useState } from "react";
 
 import googleIcon from '../assets/images/icons/google.svg';
-import facebookIcon from '../assets/images/icons/facebook.svg';
 
-const Login = () => {
+const Login = (props) => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [invalidLogin, setInvalidLogin] = useState(false);
+
+  function login() {
+    if (!email || !password) {
+      return setInvalidLogin(true);
+    }
+
+    return props.history.push('/eventos')
+  }
+
   return (
     <div>
       <div className="limiter">
@@ -11,22 +23,42 @@ const Login = () => {
           <div className="wrap-login100 p-l-50 p-r-50 p-t-77 p-b-30">
             <form className="login100-form validate-form">
               <span className="login100-form-title p-b-55">
-                Login
-          </span>
+                Área do Professor
+              </span>
 
-              <div className="wrap-input100 validate-input m-b-16" data-validate="Digite um email válido: ex@abc.xyz">
-                <input className="input100" type="text" name="email" placeholder="Email" />
+              {invalidLogin &&
+                <div style={{ margin: '0 auto', marginBottom: '5px' }}>
+                  <span style={{ color: 'red' }}>E-mail ou senha inválidos</span>
+                </div>
+              }
+
+              <div className="wrap-input100 validate-input m-b-16">
+                <input
+                  className="input100"
+                  type="text"
+                  name="email"
+                  placeholder="Email"
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
+                />
                 <span className="focus-input100"></span>
                 <span className="symbol-input100">
-                  <i class="far fa-envelope"></i>
+                  <i className="far fa-envelope"></i>
                 </span>
               </div>
 
-              <div className="wrap-input100 validate-input m-b-16" data-validate="A senha é obrigatória">
-                <input className="input100" type="password" name="pass" placeholder="Senha" />
+              <div className="wrap-input100 validate-input m-b-16">
+                <input
+                  className="input100"
+                  type="password"
+                  name="pass"
+                  placeholder="Senha"
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                />
                 <span className="focus-input100"></span>
                 <span className="symbol-input100">
-                  <i class="fas fa-unlock"></i>
+                  <i className="fas fa-unlock"></i>
                 </span>
               </div>
 
@@ -34,23 +66,23 @@ const Login = () => {
                 <input className="input-checkbox100" id="ckb1" type="checkbox" name="remember-me" />
                 <label className="label-checkbox100" htmlFor="ckb1">
                   Lembre de mim
-            </label>
+                </label>
               </div>
 
               <div className="container-login100-form-btn p-t-25">
-                <button className="login100-form-btn">
+                <button className="login100-form-btn" type="button" onClick={(e) => login(e)}>
                   Login
-            </button>
+                </button>
               </div>
 
               <div className="text-center w-full p-t-42 p-b-22">
                 <span className="txt1">
                   Ou faça login com
-            </span>
+                </span>
               </div>
 
               <a href="#" className="btn-face m-b-10">
-                <i class="fab fa-facebook-square"></i>
+                <i className="fab fa-facebook-square"></i>
                 Facebook
               </a>
 
@@ -62,11 +94,11 @@ const Login = () => {
               <div className="text-center w-full p-t-115">
                 <span className="txt1">
                   Ainda não é cadastrado?
-            </span>
+                </span>
 
                 <a className="txt1 bo1 hov1" href="#">
                   Cadastre-se agora
-            </a>
+                </a>
               </div>
             </form>
           </div>
