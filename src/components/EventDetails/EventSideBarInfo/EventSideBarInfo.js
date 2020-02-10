8c93e6bb88ae33f2edcb4a9405cc00784ca22a5c
Vitor Martins <vitor.f.martins@accenture.com>
2020-02-10 16:56:34 -0300

    Adding target audience text

--- src/components/EventDetails/EventSideBarInfo/EventSideBarInfo.js
+++ src/components/EventDetails/EventSideBarInfo/EventSideBarInfo.js
@@ -33,10 +33,17 @@ const EventSideBarInfo = ({ event }) => {
             </div>
 
             <div>
-              <h5>Detalhes</h5>
+              <h4>Detalhes</h4>
               <p style={{ textAlign: 'justify' }}>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla bibendum faucibus diam in dictum. Duis ullamcorper a justo eget aliquet. Integer ac efficitur metus. Duis molestie, mi vel vehicula auctor, nisi dolor tincidunt ipsum, eu varius elit nunc sit amet tellus. Maecenas sagittis volutpat iaculis. Mauris magna velit, porttitor eu ultricies sed, condimentum vel tortor. Cras placerat, elit tincidunt tincidunt consectetur, eros est tristique quam, ac condimentum lorem lacus in lectus. Cras neque eros, iaculis ut consectetur quis, aliquam in odio. Fusce eget est quis orci ultricies dictum a sed mauris. Vestibulum dignissim mauris in eros pulvinar commodo. Vivamus vel faucibus quam. Integer leo massa, sodales in blandit nec, faucibus quis erat. Nulla facilisi. Etiam sem urna, dignissim vel risus vitae, hendrerit gravida nunc.
-                  </p>
+              </p>
+            </div>
+
+            <div>
+              <h4>Público Alvo</h4>
+              <p style={{ textAlign: 'justify' }}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla bibendum faucibus diam in dictum. Duis ullamcorper a justo eget aliquet.
+              </p>
             </div>
           </div>
         </Col>
